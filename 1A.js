5395892abc545f9bfd2213232abc719b5b408375
Emanuel Leon-Vargas <146137985+EmanuelLeon2022@users.noreply.github.com>
2023-10-24 11:45:33 -0400

    Add files via upload

---
+++ 1A.js
@@ -0,0 +1,39 @@
+/*
+1. How do we assign a value to a variable? A. With the assignment operator
+2. How do we change the value of a...
+...
+*/
+// 1. This should declare "amogus" as 6
+let amogus = 6;
+afunction();
+
+// 2. This should change the variable "amogus" to 9
+function afunction(){
+    amogus = 9
+}
+
+// 3. this lets "sus" now be "amogus", still need to change it so we can't call back to "amogus"
+declare sus = amogus
+
+// 4.
+/*
+declare
+assign
+define
+*/
+
+// 5.
+/*
+Psuedocoding is the process of writing a setup code,
+that is not in any actual programming language,
+it allows you to test your code and your logic.
+*/
+
+// 6.
+
+/*
+Psuedocoding should roughly take about 75% of your time,
+and actual coding should take 25% of your time.
+*/
+
+console.log("The value of it is " + sus); //outputs 9
